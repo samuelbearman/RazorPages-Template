b1e72c421eddd4e75aa4d1479a6df23292735ff5
Samuel Bearman <samuel.opensource@gmail.com>
2021-07-29 22:39:28 -0400

    tag buttons and fonts

--- RazorPages/Tags/HtmlTagHelpers.cs
+++ RazorPages/Tags/HtmlTagHelpers.cs
@@ -9,7 +9,7 @@ namespace RazorPages.Tags
         public static HtmlTag PrimaryButton(this IHtmlHelper helper,
             string text,
             string type = "button",
-            string style = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full",
+            string style = "bg-blue hover:bg-blue-light text-white font-primary py-2 px-4 rounded-sm",
             Action<HtmlTag> buttonModifier = null)
         {
             buttonModifier ??= _ => { };
@@ -22,24 +22,24 @@ namespace RazorPages.Tags
             buttonModifier(buttonTag);
 
             return buttonTag;
-
-            //return new HtmlTag("button")
-            //.Attr("type", type)
-            //.AddClasses("bg-blue-500 text-white font-bold py-2 px-4 rounded-full")
-            //.AddClass("hover:bg-blue-700")
-            //.Text(text);
         }
 
-
-
         public static HtmlTag SecondaryButton(this IHtmlHelper helper,
-            string text, string type = "button")
+            string text,
+            string type = "button",
+            string style = "bg-green hover:bg-green-light text-white font-primary py-2 px-4 rounded-sm",
+            Action<HtmlTag> buttonModifier = null)
         {
-            return new HtmlTag("button")
-                .Attr("type", type)
-                .AddClasses("bg-green-500 text-white font-bold py-2 px-4 rounded-full")
-                .AddClass("hover:bg-blue-700")
-                .Text(text);
+            buttonModifier ??= _ => { };
+
+            var buttonTag = new HtmlTag("button")
+            .Attr("type", type)
+            .AddClasses(style)
+            .Text(text);
+
+            buttonModifier(buttonTag);
+
+            return buttonTag;
         }
     }
 }
