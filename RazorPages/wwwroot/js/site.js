10b8b8433a523ce212214af06f8cf740cda7fcc6
Samuel Bearman <samuel.opensource@gmail.com>
2021-08-17 19:12:04 -0400

    javascript module

--- RazorPages/wwwroot/js/site.js
+++ RazorPages/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var Dynamic = (function () {
 
-// Write your JavaScript code.
+    function init() {
+        $('#load').on('click', function (evt) {
+            evt.preventDefault();
+
+            let data = $('#formData').serialize();
+
+            $.ajax({
+                type: "POST",
+                headers: { "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val() },
+                url: 'dynamic?handler=AddItem',
+                contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+                data: data,
+                success: function (data) {
+                    $('#itemsContainer').html(data)
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log(thrownError)
+                }
+            })
+        });
+    }
+
+    return {
+        init: init
+    };
+
+})();
